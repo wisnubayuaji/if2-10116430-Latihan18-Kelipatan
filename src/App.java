d165c52c9a12a8a724f61ab3aa2c7c007210db7c
wisnubayuaji <wisnubayu34@gmail.com>
2020-11-01 07:27:15 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for (double i = 3.5; i <= 35; i = (i + 3.5)) {
+            System.out.println(i);
+        }
     }
 }
